e0f62b81ee345eceee5720d7d338a7ff1ad3ecce
David Griffiths <davidgg777@hotmail.com>
2021-11-28 09:07:28 +1100

    demo commit

--- models/wh/fct_orders.sql
+++ models/wh/fct_orders.sql
@@ -40,6 +40,7 @@ final as (
         orders.shipping_priority,
 
         1 as order_count,
+        0 as dave_count,
         order_item_summary.gross_item_sales_amount,
         order_item_summary.item_discount_amount,
         order_item_summary.item_tax_amount,
